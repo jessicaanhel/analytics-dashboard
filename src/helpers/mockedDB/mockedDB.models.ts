e9f532ecc2831f53b1487f0b7ae8f310036193da
jessicaanhel <angellas.xo@gmail.com>
2022-10-19 17:04:52 +0200

    create database (mockup)

---
+++ src/helpers/mockedDB/mockedDB.models.ts
@@ -0,0 +1,20 @@
+export interface RussianArmyTechnics {
+  monthName: string;
+  artillery: {
+    Tanks: number;
+    BBM: number;
+    Artillery: number;
+    MLRS: number;
+    AirDefenseSystems: number;
+    Aircraft: number;
+    Helicopters: number;
+    UAV: number;
+    CruiseMissiles: number;
+    ShipsBoats: number;
+    CarsAndTankTrucks: number;
+    SpecialVehicles: number;
+  };
+  personnel: {
+    staff: number;
+  };
+}
---
+++ src/helpers/mockedDB/mockedDB.ts
@@ -0,0 +1,63 @@
+export const octoberLosses = {
+  monthName: 'October',
+  artillery: {
+    // last update 19.10.22
+    Tanks: 2554,
+    BBM: 5235,
+    Artillery: 1637,
+    MLRS: 372,
+    AirDefenseSystems: 189,
+    Aircraft: 269,
+    Helicopters: 242,
+    UAV: 1286,
+    CruiseMissiles: 323,
+    ShipsBoats: 16,
+    CarsAndTankTrucks: 3999,
+    SpecialVehicles: 146,
+  },
+  personnel: {
+    staff: 66280,
+  },
+};
+
+export const septemberLosses = {
+  monthName: 'September',
+  artillery: {
+    Tanks: 2338,
+    BBM: 4932,
+    Artillery: 1391,
+    MLRS: 333,
+    AirDefenseSystems: 176,
+    Aircraft: 264,
+    Helicopters: 225,
+    UAV: 1003,
+    CruiseMissiles: 246,
+    ShipsBoats: 15,
+    CarsAndTankTrucks: 3768,
+    SpecialVehicles: 131,
+  },
+  personnel: {
+    staff: 59080,
+  },
+};
+
+export const augustLosses = {
+  monthName: 'August',
+  artillery: {
+    Tanks: 1974,
+    BBM: 4312,
+    Artillery: 1091,
+    MLRS: 285,
+    AirDefenseSystems: 152,
+    Aircraft: 234,
+    Helicopters: 204,
+    UAV: 849,
+    CruiseMissiles: 196,
+    ShipsBoats: 15,
+    CarsAndTankTrucks: 3236,
+    SpecialVehicles: 103,
+  },
+  personnel: {
+    staff: 47900,
+  },
+};
