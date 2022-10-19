997c6b86c42fe7d85ef97208afefae58fdff5910
jessicaanhel <angellas.xo@gmail.com>
2022-10-20 00:25:18 +0200

    add title to chart page

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -9,14 +9,17 @@ import { septemberLosses, octoberLosses, augustLosses} from './helpers/mockedDB/
 export const HomePage = (): JSX.Element => {
   return (
     <Box p={5}>
+      <h1>Loss of the armed forces of the Russian Federation during the hostilities against Ukraine</h1>
         <Grid container spacing={4}>
-          <Grid item xs={5}>
-            <TrioBarChart monthData={[augustLosses, septemberLosses, octoberLosses]} legendStatus = {true} />
+        <Grid item xs={5}>
+            <LineChart monthData={[augustLosses, septemberLosses, octoberLosses]} label="Loss of Russian personnel" legendStatus = {true}/>
           </Grid>
           <Grid item xs={5}>
-            <LineChart monthData={[augustLosses, septemberLosses, octoberLosses]} label="Loss of personnel"/>
+            <TrioBarChart monthData={[augustLosses, septemberLosses, octoberLosses]} legendStatus = {true} />
           </Grid>
         </Grid>
     </Box>
   );
 };
+
+export default HomePage;
