80155bd00f030221b6eae5b24b08e81b291bc010
jessicaanhel <angellas.xo@gmail.com>
2022-10-19 17:05:11 +0200

    add colors to theme

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -3,18 +3,40 @@ import { createTheme } from '@mui/material';
 export const theme = createTheme({
   palette: {
     common: {
-        black: '#121213',
-        white: '#ffffff',
-      },
+      black: '#121213',
+      white: '#ffffff',
+    },
     primary: {
-        light: '#0057B8',
-        main: '#FFD700'
+      light: '#0057B8',
+      main: '#FFD700',
+    },
+    chartColor: {
+      light: '#7DE2D1',
+      main: '#339989',
+      dark: '#2B2C28',
+      contrastText: '#121213',
+    },
+    neutral: {
+      main: 'transparent',
+      contrastText: '#121213',
     },
     grey: {
-      '100': '#f1ff1f'
-    }
+      '100': '#f1ff1f',
+    },
   },
   typography: {
     fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
   },
 });
+
+declare module '@mui/material/styles' {
+  interface Palette {
+    neutral: Palette['primary'];
+    chartColor: Palette['primary'];
+  }
+
+  interface PaletteOptions {
+    neutral?: PaletteOptions['primary'];
+    chartColor?: Palette['primary'];
+  }
+}
