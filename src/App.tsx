77efce69b4ef33906d40649234685ff672e05aca
jessicaanhel <angellas.xo@gmail.com>
2022-10-20 00:24:46 +0200

    add new router to application

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './HomePage';
 import './App.css';
+import ReactPage from './AppCopy';
+import NavigationBar from './components/NavigationBar';
+import { FilterHeader } from './components/Filtering/FilterHeader';
 
 function App() {
+  const [openFilterBar, setFilterBar] = useState(false);
+
+  const openFilterOverlay = () => setFilterBar(true);
+  const closeFilterOverlay = () => setFilterBar(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>Marine Mammals</h1>
+      <BrowserRouter>
+      <NavigationBar />
+      <Routes>
+      <Route path='/' element={<HomePage/>} />
+      <Route path="/react" element={<ReactPage />} />
+      </Routes>
+      <FilterHeader isOpen={openFilterBar} onClose={closeFilterOverlay} />
+      </BrowserRouter>
     </div>
   );
 }
