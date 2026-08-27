a0592f2871195c45111212c55a491f327b08b93f
jessicaanhel <ro.project2020@gmail.com>
2026-08-27 22:13:48 +0200

    Add 2 tabs with different charts

--- frontend/src/pages/Analytics/AnalyticsTab2.tsx
+++ frontend/src/pages/Analytics/AnalyticsTab2.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { Box, Grid } from '@mui/material';
+
+import { LineChart } from '../../components/Graphs/LineChart';
+import { TrioBarChart } from '../../components/Graphs/TrioBarChart';
+
+import { septemberLosses, octoberLosses, augustLosses} from '../../helpers/mockedDB/mockedDB';
 
 const AnalyticsTab2 = () => {
-  return <h2>AnalyticsTab Tab 2 content goes here</h2>;
+  return (
+      <Box p={3}>
+          <h1>War loss</h1>
+          <Grid container spacing={4}>
+            <Grid item xs={12} md={6}>
+              <LineChart
+                monthData={[augustLosses, septemberLosses, octoberLosses]}
+                label="Loss of personnel"
+                legendStatus={true}
+              />
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <TrioBarChart monthData={[augustLosses, septemberLosses, octoberLosses]} legendStatus={true} />
+            </Grid>
+          </Grid>
+        </Box>
+      )
 };
 
 export default AnalyticsTab2;
